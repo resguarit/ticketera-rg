57d888d1016395ddeb25e33727211196dbb596aa
moragentil <moragentil@gmail.com>
2025-08-06 10:45:51 -0300

    pagina de eventos

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/events', function () {
-    return Inertia::render('events');
+    return Inertia::render('public/events');
 })->name('events');
 
 
