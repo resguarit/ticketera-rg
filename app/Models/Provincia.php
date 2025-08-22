617c22ded49b935f6930a71e780ee9a0cf25e815
Mariano <marianosalas24@gmail.com>
2025-08-22 09:04:59 -0300

    feat: update venues method to use HasManyThrough relationship for better data retrieval

--- app/Models/Provincia.php
+++ app/Models/Provincia.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Provincia extends Model
 {
@@ -25,7 +26,7 @@ class Provincia extends Model
         return $this->hasMany(Ciudad::class);
     }
 
-    public function venues(): HasMany
+    public function venues(): HasManyThrough
     {
         return $this->hasManyThrough(Venue::class, Ciudad::class);
     }
