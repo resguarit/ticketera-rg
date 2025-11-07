6edf1bc69a96d7e65fdb7cc88ca6768a8719a98b
Mariano Salas Triana <marianosalas24@gmail.com>
2025-11-07 10:50:38 -0300

    Update Decidir URL to production endpoint
    cambiar la url a prod

--- resources/js/pages/public/newcheckoutconfirm.tsx
+++ resources/js/pages/public/newcheckoutconfirm.tsx
@@ -88,8 +88,8 @@ export default function CheckoutConfirm({ eventData, eventId, sessionId, lockExp
 
   useEffect(() => {
     if (!decidirSandboxRef.current) {
-      const urlSandbox = "https://developers-ventasonline.payway.com.ar/api/v2"
-      decidirSandboxRef.current = new (window as any).Decidir(urlSandbox)
+      const urlProd = "https://live.decidir.com/api/v2/"
+      decidirSandboxRef.current = new (window as any).Decidir(urlProd)
       decidirSandboxRef.current.setPublishableKey("9960377671874d4fb71d0a8448642730")
     }
   }, [])
