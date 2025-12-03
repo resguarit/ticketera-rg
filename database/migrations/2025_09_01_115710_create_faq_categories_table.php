108f9471d0c864aedf372a5a312cfdc5408d701c
Mariano <marianosalas24@gmail.com>
2025-12-03 11:05:03 -0300

    migraciones que faltan

--- database/migrations/2025_09_01_115710_create_faq_categories_table.php
+++ database/migrations/2025_09_01_115710_create_faq_categories_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // tiene que estar
         Schema::create('faq_categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
--- database/migrations/2025_09_01_115810_create_faqs_table.php
+++ database/migrations/2025_09_01_115810_create_faqs_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // tiene que estar
         Schema::create('faqs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('faq_category_id')->constrained()->onDelete('cascade');
