c43f49904b86b1d4793a9bcefd69a2213591407b
moragentil <moragentil@gmail.com>
2025-12-12 10:04:34 -0300

    unique en factory de provincia

--- database/factories/ProvinciaFactory.php
+++ database/factories/ProvinciaFactory.php
@@ -17,7 +17,7 @@ class ProvinciaFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->state,
+            'name' => $this->faker->unique()->state,
             'country' => 'Argentina',
         ];
     }
